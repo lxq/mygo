5790d1f2a8804e24d4c551de7359ba657ab6bf0e
lxq <linxq@live.com>
2013-11-05 09:22:53 +0800

    增加结构及其方法的练习。

--- src/hellogo/main.go
+++ src/hellogo/main.go
@@ -6,7 +6,18 @@ import (
 	"mymath"
 )
 
+type Rect struct {
+	w, h int
+}
+
+func (rc Rect) Area() int {
+	return rc.w * rc.h
+}
+
 func main() {
 	fmt.Println("Hello World! 你好，世界！")
 	fmt.Printf("2的开根号：%v \n", mymath.Sqrt(2))
+	
+	r1 := Rect{123, 456}
+	fmt.Printf("面积是：%d\n", r1.Area())
 }
